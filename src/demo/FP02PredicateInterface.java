70d64fc4a0eef019d8c5e917d6ecce6abe41f7f4
upillai <!5D6SvJ3vbr1>
2022-02-01 16:29:53 -0600

    Added Function class

--- src/demo/FP02PredicateInterface.java
+++ src/demo/FP02PredicateInterface.java
@@ -2,6 +2,7 @@ package demo;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 class EvenNumberPredicate implements Predicate<Integer> {
@@ -17,6 +18,13 @@ class SystemOutConsumer implements Consumer<Integer>{
         System.out.println(number);
     }
 }
+
+class NumberSquareMapper implements Function<Integer, Integer>{
+    @Override
+    public Integer apply(Integer num) {
+        return num * num;
+    }
+}
 public class FP02PredicateInterface {
     public static void main(String[] args) {
         /*
@@ -26,7 +34,9 @@ public class FP02PredicateInterface {
          */
         List.of(2, 3, 4, 5, 6).stream()
                 .filter(n -> n % 2 == 0)
+                .map(n->n*n)
                 .forEach(e -> System.out.println(e));
+
         List.of(2, 3, 4, 5, 6).stream()
                 .filter(new EvenNumberPredicate()) //    Stream<T> filter(Predicate<? super T> predicate);
                 .forEach(e -> System.out.println(e));
@@ -34,6 +44,11 @@ public class FP02PredicateInterface {
         List.of(2, 3, 4, 5, 6).stream()
                 .filter(n -> n % 2 == 0)
                 .forEach(new SystemOutConsumer());//     void forEach(Consumer<? super T> action);
+
+        List.of(2, 3, 4, 5, 6).stream()
+                .filter(n -> n % 2 == 0)
+                .map(new NumberSquareMapper()) //     <R> Stream<R> map(Function<? super T, ? extends R> mapper);
+                .forEach(e -> System.out.println(e));
 
     }
 }
