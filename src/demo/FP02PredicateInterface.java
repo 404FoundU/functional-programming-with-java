a90b315779d64554778c6acdd2ba0d46a01b49f4
upillai <!5D6SvJ3vbr1>
2022-02-01 16:22:19 -0600

    Added Consumer class

--- src/demo/FP02PredicateInterface.java
+++ src/demo/FP02PredicateInterface.java
@@ -1,6 +1,7 @@
 package demo;
 
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 class EvenNumberPredicate implements Predicate<Integer> {
@@ -9,13 +10,30 @@ class EvenNumberPredicate implements Predicate<Integer> {
         return number % 2 == 0;
     }
 }
+
+class SystemOutConsumer implements Consumer<Integer>{
+    @Override
+    public void accept(Integer number) {
+        System.out.println(number);
+    }
+}
 public class FP02PredicateInterface {
     public static void main(String[] args) {
+        /*
+        Compiler creates an impl of FunctionalInterface( Predicate<Integer>)
+        FunctionalInterface will have one method without any definition ( test )
+        Lambda expression provides an impl for the specific method
+         */
         List.of(2, 3, 4, 5, 6).stream()
                 .filter(n -> n % 2 == 0)
                 .forEach(e -> System.out.println(e));
         List.of(2, 3, 4, 5, 6).stream()
-                .filter(new EvenNumberPredicate())
+                .filter(new EvenNumberPredicate()) //    Stream<T> filter(Predicate<? super T> predicate);
                 .forEach(e -> System.out.println(e));
+
+        List.of(2, 3, 4, 5, 6).stream()
+                .filter(n -> n % 2 == 0)
+                .forEach(new SystemOutConsumer());//     void forEach(Consumer<? super T> action);
+
     }
 }
