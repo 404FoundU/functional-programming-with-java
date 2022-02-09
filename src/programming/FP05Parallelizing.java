d0e7ba93e522190e4ca880221b70137fe778bf91
upillai <!5D6SvJ3vbr1>
2022-02-08 18:46:49 -0600

    added parallel()

--- src/programming/FP05Parallelizing.java
+++ src/programming/FP05Parallelizing.java
@@ -1,5 +1,6 @@
 package programming;
 
+import java.util.List;
 import java.util.stream.LongStream;
 
 public class FP05Parallelizing {
@@ -14,6 +15,10 @@ public class FP05Parallelizing {
 		System.out.println(LongStream.range(0,1000000000).parallel().sum());
 		
 		System.out.println(System.currentTimeMillis() - time);
+		List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+		int sum = numbers.stream()
+				.parallel() // splits the stream across processor
+				.reduce(0, (num1, num2) -> num1 + num2);
 	}
 
 }
